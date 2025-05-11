c60b764d6eaf0bf86c6b32e85ad7bbc8c0ecd87b
Meymat <meymatinfar@gmail.com>
2025-05-11 23:03:10 +0330

    Update Dockerfile

--- modules/order/routes/v1/api.php
+++ modules/order/routes/v1/api.php
@@ -9,3 +9,7 @@ Route::controller(OrderController::class)->middleware('auth:sanctum')->prefix('o
     Route::get('history', 'history');
     Route::post('cancel', 'cancel');
 });
+
+Route::get('test-redis', function(){
+    Cache::store('redis')->put('test-key', ['foo' => 'bar'], 60);
+});
